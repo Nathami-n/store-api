f34633c19a4fdc9be05b01def341725518a54df6
Nathan George <georgenathan010@gmail.com>
2024-02-10 21:24:38 +0300

    added a simple server

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('04 Store API')
+const express = require("express");
+const { loggerItemFunction } = require("./middleware/logEventsMiddleware");
+const errorHandler = require("./middleware/errorEventEmitter");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+
+//server static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//parse json data
+app.use(express.json());
+
+//middleware for logging events
+app.use(loggerItemFunction);
+
+app.use('/', (req,res)=> {
+    req.status(200).json({message:'success'})
+})
+
+//logError if it occurs
+app.use(errorHandler);
+app.listen(PORT, () => {
+  `listening on port ${PORT}`;
+});

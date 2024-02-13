816da367afe606b46d95fe21ee936bfba72d7ddf
Nathan George <georgenathan010@gmail.com>
2024-02-13 21:04:54 +0300

    implemented the sorting algorithm

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,14 +1,26 @@
 const Product = require("../models/productsModel");
 const getAllProducts = async (req, res) => {
-  const { featured } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
-    products = await Product.find(queryObject);
-    return res.status(200).json({ products, nbHits: products.length });
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
   }
 
-  products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 

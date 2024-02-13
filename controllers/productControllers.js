aa48dc9ffcb039fee4ddd2654862f3082b6b4150
Nathan George <georgenathan010@gmail.com>
2024-02-13 21:52:10 +0300

    added the skip and pagination logic

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productsModel");
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields, limit } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -23,9 +23,12 @@ const getAllProducts = async (req, res) => {
     const fieldList = fields.split(",").join(" ");
     result = result.select(fieldList);
   }
-  if (limit) {
-    result = result.limit(Number(limit))
-  }
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
